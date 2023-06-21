bb5ecf0cc42515236e57a7346d3608ad00ad216e
Giovanna Florin <80653026+GiFlorin@users.noreply.github.com>
2023-06-21 20:42:11 -0300

    Update cronometromau.py

--- corrigirchatgpt/cronometromau.py
+++ corrigirchatgpt/cronometromau.py
@@ -1,5 +1,7 @@
 #Erro: Eu pedi para o chat gpt programar um cronometro, mas ele programou um timer, pois nós colocamos o tempo desejado no início
 # quando em um cronometro se começa do zero para cima.
+
+#Isso provavelmente foi causado pelo motivo de não dar para parar o cronometro no meio da contagem, então ele tentou achar outra forma de fazer o código
 import time
 
 def cronometro(tempo):
